125f94b989381c555f9881923b23f6234e4beb3f
tremmertt <hoangtrammm98@gmail.com>
2022-03-09 22:27:33 +0700

    fix header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import "./Responsive.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Service from "./pages/Service/Service";
 
@@ -17,16 +22,67 @@ import DetailNews from "./components/News/DetailNews";
 
 function App() {
   const [offset, setOffset] = useState(window.pageYOffset);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const currentWidthScreen = window.innerWidth;
+  const location = window.location;
+  console.log(location.pathname);
+
+  // function HomeButton() {
+  //   let history = useHistory();
+
+  //   function handleClick() {
+  //     history.push("/home");
+  //   }
+  // }
+
+  // const getRoutePath = (location, params) => {
+  //   const { pathname } = location;
+
+  //   if (!Object.keys(params).length) {
+  //     return pathname; // we don't need to replace anything
+  //   }
+
+  //   let path = pathname;
+  //   Object.entries(params).forEach(([paramName, paramValue]) => {
+  //     if (paramValue) {
+  //       path = path.replace(paramValue, `:${paramName}`);
+  //     }
+  //   });
+  //   return path;
+  // };
 
   useEffect(() => {
     // console.log("useEffect");
-    const onScroll = () => setOffset(window.pageYOffset);
+    const onScroll = () => {
+      setIsScrolled(true);
+      setOffset(window.pageYOffset);
+    };
     window.removeEventListener("scroll", onScroll);
     window.addEventListener("scroll", onScroll, { passive: true });
     return () => {
       window.removeEventListener("scroll", onScroll);
     };
   }, []);
+
+  function isDisplayHeader() {
+    //     - màn hình nhỏ hơn 1024
+    if (currentWidthScreen < 1024) {
+      return true;
+    } else {
+      if (isScrolled) {
+        return true;
+      } else {
+        // - manf hinhf lon hon 1024
+        // - vừa vào màn hìn - chưa scroll lần nào
+        // - phải ở trang home
+        if (location.pathname !== "/") {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
 
   function scrollToTop(scrollDuration) {
     var scrollStep = -window.scrollY / (scrollDuration / 15),
@@ -69,7 +125,11 @@ function App() {
         </button>
       )}
       <Router>
-        <Header className="w-screen" scrollToTop={scrollToTop} />
+        {isDisplayHeader() ? (
+          <Header className="w-screen" scrollToTop={scrollToTop} />
+        ) : (
+          <div></div>
+        )}
         <Routes>
           <Route path="/" element={<Home />} animate={true} />
           <Route path="/about" element={<About />} animate={true} />
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
+import React, { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
 
 export default function Carousel() {
+  const [curHeight, setCurrentHeight] = useState(window.innerHeight);
+  const [curWidth, setCurrentWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // console.log("useEffect");
+    const onResizeHeight = () => setCurrentHeight(window.innerHeight);
+    const onResizeWidth = () => setCurrentWidth(window.innerWidth);
+    // clean up code
+    window.removeEventListener("resize", onResizeHeight);
+    window.addEventListener("resize", onResizeHeight, { passive: true });
+
+    window.removeEventListener("resize", onResizeWidth);
+    window.addEventListener("resize", onResizeWidth, { passive: true });
+    return () => {
+      window.removeEventListener("resize", onResizeHeight);
+      window.removeEventListener("resize", onResizeWidth);
+    };
+  }, []);
+
+  console.log(curHeight, curWidth);
   return (
-    <div className='container-fluid videoPlay'>
-        <ReactPlayer url="https://www.glassegg.com/wp-content/themes/glassegg/assets/images/ge-intro.mp4" width={'100%'} height={'100%'} fluid='true' pip={true}  playing={true} muted={true} loop={true} />
+    <div className="container-fluid videoPlay ">
+      <ReactPlayer
+        url="https://www.glassegg.com/wp-content/themes/glassegg/assets/images/ge-intro.mp4"
+        width={curWidth > 1024 ? curWidth : "100%"}
+        height={curWidth > 1024 ? (curHeight > 850 ? 850 : curHeight) : "100%"}
+        fluid="true"
+        pip={true}
+        playing={true}
+        muted={true}
+        loop={true}
+      />
     </div>
-  )
+  );
 }
